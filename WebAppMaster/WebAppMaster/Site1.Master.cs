e105fbd1530190d2bdf26add4ee914bff742b1f3
Amulya <mangalagiriamulya18@gmail.com>
2023-08-26 12:01:21 +0530

    first Commit

---
+++ WebAppMaster/WebAppMaster/Site1.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppMaster
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
